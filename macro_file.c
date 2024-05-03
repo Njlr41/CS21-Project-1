74516a68221953b1c9689510d272840fc91da31d
Njlr41 <nicholasjohnreyes@gmail.com>
2024-05-03 11:00:12 +0800

    fix

--- macro_file.c
+++ macro_file.c
@@ -5,7 +5,7 @@ int GCD(int x, int y);
 void arrow(int a, int b);
 void print_str(char* string);
 void read_str(); // Ask Ma'am regarding parameters
-void print_integer(int int);
+void print_integer(int number);
 void read_integer();
 void exit();
 
