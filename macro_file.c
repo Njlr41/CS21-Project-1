b8be2471e9d07411a22f0eca0ecafdf8c6c35802
Njlr41 <nicholasjohnreyes@gmail.com>
2024-05-08 20:11:16 +0800

    Updated macro_file.c

--- macro_file.c
+++ macro_file.c
@@ -1,12 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int GCD(int x, int y);
-void arrow(int a, int b);
-void print_str(char* string);
-void read_str(); // Ask Ma'am regarding parameters
-void print_integer(int number);
-void read_integer();
+int GCD(int x, int y){
+  for(x,y; y!=0;){
+   int temp = x;
+   x = y;
+   y = temp%y;
+  } 
+  return x;
+};
+void print_str(char* string){
+  printf("%s", string);
+}
+void read_str(){
+
+} // Ask Ma'am regarding parameters
+void print_integer(int number){
+  printf("%d", number);
+}
+void read_integer(int number){
+  scanf("%d", &number);
+}
 void exit();
 
 
